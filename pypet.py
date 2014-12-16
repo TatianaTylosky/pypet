54b84838d92cdde7eb79114d7642f97491fa6120
Tatiana Tylosky <tatianatylosky@gmail.com>
2014-12-16 11:31:21 -0500

    initial commit

---
+++ pypet.py
@@ -0,0 +1,31 @@
+print 'Welome to Pypet!'
+
+cat = {
+  'name': 'Mr Fluffy',
+  'hungry': True,
+  'weight': 9.5,
+  'number_of_toys': 5,
+  'photo': '(=^o.o^=)__',
+}
+
+mouse = {
+  'name': 'Mouse',
+  'hungry': False,
+  'weight': 1.5,
+  'number_of_toys': 6,
+  'photo': '<:3 )~~~~',
+}
+
+pets = [cat, mouse]
+
+def feed(cat):
+	if cat['hungry']:
+		cat['hungry'] = False
+		cat['weight']+=1
+	else:
+		print 'The Pypet is not hungry!'
+
+for pet in pets:
+  feed(pet)
+  print pet
+
